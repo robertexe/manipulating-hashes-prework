b94e8f75147e405e2aa4f0bb5ce4b913acb59252
Robert Tabares <robtabares92@gmail.com>
2018-05-25 20:45:51 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,8 +6,8 @@ def second_challenge
    grains: ["rice", "pasta"]
   }
 
-  #code your solution here!
-
-  
+  # output = groceries.values
+  # output.flatten!
+  return groceries.values.flatten
 
 end
